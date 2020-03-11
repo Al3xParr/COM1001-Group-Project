0809747c55bbecf27b7ec9803e39267a6fd0d31b
Luke Suckling <lsuckling1@sheffield.ac.uk>
2020-03-11 12:13:52 +0000

    Added login functionality to login controller.

--- src/controllers/Login.rb
+++ src/controllers/Login.rb
@@ -4,10 +4,12 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative 'models/User'
 
 # return the login page
 get '/login' do
     
+    erb: "Login/index.erb"
 end
 
 # post login form data, evaluate and set session values as required
@@ -16,9 +18,23 @@ end
 #   password
 post '/login' do
     
+    @username = params[:username]
+    @password = params[:password]
+    
+    success = User.authenticate(@username, @password)
+    
+    if success then
+        session[:loggedIn] = true
+        redirect '/bookmarks'
+    end
+        
+    @loginError = "Username or password isn't correct."
+    redirect '/login'
 end
 
 # logout the currently logged in user
 post '/login/logout' do
-
+    
+    session[:loggedIn] = false
+    redirect '/login'
 end
