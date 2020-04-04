645eba6cf97e67d339d11570e31d515dbd93630a
Yaxin Ding <yding41@sheffield.ac.uk>
2020-04-04 00:28:02 +0000

    completed basic functionality of the edit page - can edit description of bookmark

--- src/controllers/Bookmarks.rb
+++ src/controllers/Bookmarks.rb
@@ -10,14 +10,15 @@ set :bind, '0.0.0.0'
 set :views, '../views'
 enable :sessions
 
+@BOOKMARK_ID = Bookmark.getAllId
+
 # show all avaliable bookmarks
 # /bookBookmark.getTitle in order of date of creation
 get '/bookmarks/all' do
-    #$TITLE = ["APPLE", "BANANA", "PEAR"]
     # gets all info about the bookmark
-    $TITLE = Bookmark.getAll
+    @TITLE = Bookmark.getAll
     # numbers each bookmark
-    $TITLE_LENGTH = (0...$TITLE.length).to_a
+    @TITLE_LENGTH = (0...@TITLE.length).to_a
     erb :"Bookmark/index"
 end
 
@@ -29,11 +30,12 @@ end
 
 #post search form data and perform search using values
 post '/bookmarks/search' do
-    session[:search] = params[:search]
-    $SEARCH = session[:search] #takes the search work from the search form
-    if $SEARCH != '' #if search form is not empty
-        $SEARCH_RESULTS = Bookmark.search($SEARCH) 
-        $SEARCH_LENGTH = (0...$SEARCH_RESULTS.length).to_a
+    session[:search] = params[:search] 
+    session[:bookmarkId] = params[:bookmarkId]
+    @SEARCH = session[:search] #takes the search work from the search form
+    if @SEARCH != '' #if search form is not empty
+        @SEARCH_RESULTS = Bookmark.search(@SEARCH) 
+        @SEARCH_LENGTH = (0...@SEARCH_RESULTS.length).to_a
     end
     erb :"Bookmark/search_results"  
 end
@@ -47,10 +49,17 @@ end
 # update the defined bookmark with the parsed post data
 # /bookmarks/edit
 post '/bookmarks/edit' do
+    session[:edit] = params[:edit]
+    session[:bookmarkId] = params[:bookmarkId]
     @BOOKMARK = session[:bookmarkId]
     @DESCRIPTION = session[:edit]
     @UPDATED = Bookmark.updateDescription(@DESCRIPTION, @BOOKMARK)
+    #erb :"Bookmark/edit_results"
+    redirect "bookmarks/all"
 end
+
+
+
 
 # view the bookmarks own page
 # /bookmarks/view?id={bookmark id}
