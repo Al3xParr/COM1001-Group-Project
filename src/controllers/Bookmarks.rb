df40f2b21958aa42c819e69e3ab65f8964c5dc1b
Yaxin Ding <yding41@sheffield.ac.uk>
2020-04-05 17:40:58 +0000

    can now search by title, bookmarkId and userId

--- src/controllers/Bookmarks.rb
+++ src/controllers/Bookmarks.rb
@@ -10,15 +10,15 @@ set :bind, '0.0.0.0'
 set :views, '../views'
 enable :sessions
 
-
+@search_options = ['Ti' => 'Title', 'Ta' => 'Tags', 'B' => 'Bookmark ID', 'U' => 'User ID']
 
 # show all avaliable bookmarks
 # /bookBookmark.getTitle in order of date of creation
 get '/bookmarks/all' do
     # gets all info about the bookmark
-    @TITLE = Bookmark.getAll
+    @title = Bookmark.getAll
     # numbers each bookmark
-    @TITLE_LENGTH = (0...@TITLE.length).to_a
+    @title_length = (0...@title.length).to_a
     erb :"Bookmark/index"
 end
 
@@ -30,11 +30,19 @@ end
 
 #post search form data and perform search using values
 post '/bookmarks/search' do
-    session[:search] = params[:search].strip
-    session[:bookmarkId] = params[:bookmarkId]
-    @SEARCH = session[:search] #takes the search work from the search form
-    if @SEARCH != '' #if search form is not empty
-        @SEARCH_RESULTS = Bookmark.search(@SEARCH) 
+    session[:search] = params[:search]
+    @search_option = params[:search_option]
+    @search = session[:search] #takes the search work from the search form
+    if @search != '' #if search form is not empty
+        if @search_option == "Title"
+            @search_results = Bookmark.searchByTitle(@search) 
+#         elsif @search_option == "Tags"
+#             @search_results = Bookmark.searchByTags(@search)
+        elsif @search_option == "Bookmark ID"
+            @search_results = Bookmark.searchByBookmarkId(@search)
+        elsif @search_option == "User ID"
+            @search_results = Bookmark.searchByUserId(@search)
+        end
     end
     erb :"Bookmark/search_results"  
 end
@@ -50,14 +58,16 @@ end
 # update the defined bookmark with the parsed post data
 # /bookmarks/edit
 post '/bookmarks/edit' do
+    # assigned to variable in case we want to display edited
+    # somewhere in the future as confirmation page
     if params[:title] != ''
-        @TITLE = Bookmark.updateTitle(params[:title], $BOOKMARK_ID)
+        @title = Bookmark.updateTitle(params[:title], $BOOKMARK_ID)
     end
     if params[:edit] != ''
-        @DESCRIPTION = Bookmark.updateDescription(params[:edit], $BOOKMARK_ID)
+        @description = Bookmark.updateDescription(params[:edit], $BOOKMARK_ID)
     end
     if params[:resource] != ''
-        @RESOURCE = Bookmark.updateResource(params[:resource], $BOOKMARK_ID)
+        @resource = Bookmark.updateResource(params[:resource], $BOOKMARK_ID)
     end
     #erb :"Bookmark/edit_results"
     redirect "bookmarks/all"
@@ -68,7 +78,10 @@ end
 
 # view the bookmarks own page
 # /bookmarks/view?id={bookmark id}
-get '/bookmarks/view' do
+get '/bookmarks/view/:bookmarkId' do
+    @view_bookmark_row_titles = ["Created at: ", "Title: ", "Description: ", "Resource: ", "Archived: ", "User ID: "]
+    @view_bookmark = Bookmark.getInfo(params[:bookmarkId])
+    @view_bookmark_length = (0...@view_bookmark.length).to_a
     erb :"Bookmark/view"
 end
 
