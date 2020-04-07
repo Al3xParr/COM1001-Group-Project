38e5be8f035a7c149e14f8fa295d86cd2f0cd66b
lukes00 <38917968+lukes00@users.noreply.github.com>
2020-04-07 13:13:34 +0100

    Improved styling of indiidual bookmarks page. Added comments and none functional buttons.

--- src/seedDB.rb
+++ src/seedDB.rb
@@ -1,4 +1,4 @@
 require_relative 'models/User'
 require_relative 'models/Bookmark'
 
-puts Bookmark.newBookmark("Test", "Test description", "https://google.co.uk/", 0, 1)
+puts Bookmark.newBookmark("Staff payroll 2020", "This is the staff payrol for the year blah blah blah", "https://some.internal.server/payroll/2020.xlsx/", 0, 1)
