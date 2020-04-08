69b5d37fb16ca9f864b5ec4b9f1cd0be7156bbbf
lukes00 <38917968+lukes00@users.noreply.github.com>
2020-04-08 13:46:38 +0100

    Added comments controller.

---
+++ src/controllers/Comments.rb
@@ -0,0 +1,19 @@
+# Comments.rb
+# Author: Luke Suckling
+# Date: 08/04/2020
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+post 'comments/create' do
+
+end
+
+post 'comments/edit/:commentId' do
+
+end
+
+post 'comments/delete/:commentId' do
+
+end
