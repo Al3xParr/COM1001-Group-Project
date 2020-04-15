0bb72f98ac261729acaedc2ea6f4db17ee00740d
lukes00 <38917968+lukes00@users.noreply.github.com>
2020-04-15 10:58:50 +0100

    Added tags controller, removed old form as mentioned in prev commit. Update DB schema.

---
+++ src/controllers/Tags.rb
@@ -0,0 +1,17 @@
+# Tags.rb
+# Author: Luke Suckling
+# Date: 15/04/2020
+
+require 'sinatra'
+require 'sinatra/reloader'
+require_relative '../models/Tag'
+
+post '/tags/add' do
+
+    
+
+end
+
+post '/tags/delete' do
+
+end
