aba3a1faed0032d0272ad6e83995c9e21192cbba
Alexander Parr <ajparr1@sheffield.ac.uk>
2020-05-20 10:22:04 +0000

    Added some comments

--- src/controllers/Admin.rb
+++ src/controllers/Admin.rb
@@ -1,11 +1,11 @@
 # Admin.rb
-# Author: Luke Suckling
-# Date: 09/03/2020
+# Alex Parr
+# 20/05/2020
 
 require 'sinatra'
 require 'sinatra/reloader'
 
-# fetch the admin 'homepage'
+
 # /admin
 get '/admin' do
     if session[:admin] then
