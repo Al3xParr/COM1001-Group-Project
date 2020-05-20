8b992de922e5c4b9272c1292d138e12b19b30b18
Alexander Parr <ajparr1@sheffield.ac.uk>
2020-05-20 10:20:12 +0000

    Created admin page and placeholder admin pages

--- src/controllers/Admin.rb
+++ src/controllers/Admin.rb
@@ -8,10 +8,35 @@ require 'sinatra/reloader'
 # fetch the admin 'homepage'
 # /admin
 get '/admin' do
-    
     if session[:admin] then
-        "hello mr admin"
+        erb :"Admin/index"
     else
-        status 404
+        redirect :"bookmarks/all"
     end
+end
+
+get '/admin/bookmarkReport' do
+    if session[:admin] then
+        erb :"Admin/bookmarkReport"
+    else
+        redirect :"bookmarks/all"
+    end
+end
+
+get '/admin/userApproval' do
+        if session[:admin] then
+        erb :"Admin/userApproval"
+    else
+        redirect :"bookmarks/all"
+    end
+   
+end
+
+get '/admin/userDisable' do
+    if session[:admin] then
+        erb :"Admin/userDisable"
+    else
+        redirect :"bookmarks/all"
+    end
+    
 end
