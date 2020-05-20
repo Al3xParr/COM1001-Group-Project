6896d2c56ed73ad2172b732c8a87b4093d355d34
Ramona Petre <rampetre1@sheffield.ac.uk>
2020-05-20 12:39:40 +0000

    New page for the reports created. Need to be working on the delete button for each report.

--- src/controllers/Admin.rb
+++ src/controllers/Admin.rb
@@ -4,14 +4,25 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+require_relative '../models/Bookmark'
+require_relative '../models/BookmarkReport'
 
 # fetch the admin 'homepage'
 # /admin
 get '/admin' do
     
     if session[:admin] then
-        "hello mr admin"
+        @bookmarksreports = BookmarkReport.getAll()
+        erb :"Admin/reports"
     else
         status 404
     end
+end
+    
+get '/admin/view:reportId' do
+    #fetch bookmark object
+    @reportId = BookmarkReport.getById(params[:bookmarkId])
+
+    erb :"/Admin/view"
 end
