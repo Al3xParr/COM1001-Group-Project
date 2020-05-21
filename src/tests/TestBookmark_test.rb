48378cda9946ea8412dc062f97c85edd55b0c181
Ramona Petre <rampetre1@sheffield.ac.uk>
2020-05-21 17:20:05 +0000

    Comments added.

--- src/tests/TestBookmark_test.rb
+++ src/tests/TestBookmark_test.rb
@@ -19,7 +19,9 @@ class BookmarksTests < Minitest::Test
         db.execute "INSERT INTO bookmarks(createdAt, title, description, resource, archived) VALUES('13 March', 'movies', 'anything', 'none', 0);" 
         
     end
-
+    
+    #Gets all the bookmarks from the database
+    #Should return true if the length is 4
     def test_get_all_bookmarks
 
         bookmarks = Bookmark.getAll()
@@ -28,6 +30,8 @@ class BookmarksTests < Minitest::Test
         
     end
 
+    #Gets the first bookmark from the database: created at = 18 March, title = apple, description = something, resource = something, archived = true
+    #Should return for all the tests
     def test_get_all_bookmark_details
         
         bookmarks = Bookmark.getAll()
@@ -42,6 +46,8 @@ class BookmarksTests < Minitest::Test
 
     end
 
+    #Adds bookmarks into the database, one that respects the field requiremenets and one that is "archived"
+    #Should return true for the first bookmark because it respects the requiremenets and false for the second one
     def test_new_bookmark
 
         resultOne = Bookmark.newBookmark("Hello", "Hello descriptiption", "https://google.co.uk/", 1, 1);
@@ -54,6 +60,8 @@ class BookmarksTests < Minitest::Test
 
     end
 
+    #Gets a bookmark that has ID = 1 and a bookmark that has ID = -1
+    #Should return true for all the tests made for ID = 1 and ID = -1, because there does not exist a bookmark with a negative ID
     def test_get_by_id
 
         bookmarkOne = Bookmark.getById(1)
