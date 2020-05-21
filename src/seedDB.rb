cf07576e5a664088f6063701ad0ec9083d826b6a
Luke Suckling <lsuckling1@sheffield.ac.uk>
2020-05-21 14:53:55 +0000

    Seeded db with required user accounts as in requirements document.

--- src/seedDB.rb
+++ src/seedDB.rb
@@ -3,5 +3,9 @@ require_relative 'models/Bookmark'
 require_relative 'models/SignupRequest'
 
 #puts Bookmark.newBookmark("Staff payroll 2020", "This is the staff payrol for the year blah blah blah", "https://some.internal.server/payroll/2020.xlsx/", 0, 1)
-#puts User.newUser("nathan", "password", 1, 0)
-#puts User.newUser("luke", "password", 1, 0)
+puts User.newUser("admin", "admin", 1, 0)
+puts User.newUser("user", "user", 0, 0)
+puts User.newUser("role1", "role1", 0, 0)
+puts User.newUser("role2", "role2", 0, 0)
+puts User.newUser("role3", "role3", 0, 0)
+puts User.newUser("role4", "role4", 0, 0)
